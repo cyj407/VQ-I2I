ee76f6409dbcf29f91520a519d5cc3fd3aeb5932
SHINICHENG <jenny0870207@gmail.com>
2021-08-03 21:16:52 +0800

    turn off flipping when processing pair data

--- dataset.py
+++ dataset.py
@@ -215,8 +215,7 @@ class dataset_pair(data.Dataset):
         else:
             transforms.append(CenterCrop(cropsize))
 
-        ## flip
-        transforms.append(RandomHorizontalFlip())
+     
 
         transforms.append(ToTensor())
         transforms.append(Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]))
