53169847b049af1c6bd95c991df8663a221bf506
cyj407 <dfghj345678@gmail.com>
2021-08-24 01:48:22 +0800

    add style patch GAN

---
+++ settingc_styleGAN_train.py
@@ -0,0 +1,375 @@
+import argparse, os, sys, datetime, glob, importlib
+from omegaconf import OmegaConf
+import numpy as np
+from PIL import Image
+import torch
+import torchvision
+from torch.utils.data import random_split, DataLoader, Dataset
+import torch.nn.functional as F
+from dataset import dataset_combine, dataset_unpair
+from torch.utils.data import DataLoader
+import os
+
+from taming_comb.modules.style_encoder.network import *
+from taming_comb.modules.diffusionmodules.model import * 
+
+import argparse
+
+def get_obj_from_str(string, reload=False):
+    module, cls = string.rsplit(".", 1)
+    if reload:
+        module_imp = importlib.import_module(module)
+        importlib.reload(module_imp)
+    return getattr(importlib.import_module(module, package=None), cls)
+
+
+
+def instantiate_from_config(config):
+    if not "target" in config:
+        raise KeyError("Expected key `target` to instantiate.")
+    return get_obj_from_str(config["target"])(**config.get("params", dict()))
+
+
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("device",
+                    help="specify the GPU(s)",
+                    type=str)
+
+    parser.add_argument("dataset",
+                    help="dataset",
+                    type=str)
+    
+    parser.add_argument("ne",
+                    help="the number of embedding",
+                    type=int)
+
+    parser.add_argument("ed",
+                    help="embedding dimension",
+                    type=int)
+
+    parser.add_argument("z_channel",
+                    help="z channel",
+                    type=int)
+    
+
+    parser.add_argument("epoch_start",
+                    help="start from",
+                    type=int)
+
+    parser.add_argument("epoch_end",
+                    help="end at",
+                    type=int)
+
+    args = parser.parse_args()
+
+    os.environ["CUDA_VISIBLE_DEVICES"]=args.device
+
+    # ONLY MODIFY SETTING HERE
+    device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+    print('device: ', device)
+    batch_size = 1 # 128
+    learning_rate = 1e-5       # 256/512 lr=4.5e-6 from 71 epochs
+    ne = args.ne  # Enlarge
+    ed = args.ed
+    img_size = 256
+    epoch_start = args.epoch_start
+    epoch_end = args.epoch_end
+    switch_weight = 0.1 # self-reconstruction : a2b/b2a = 10 : 1
+    
+    
+    # save_path = 'both_afhq_{}_{}_rec_switch_img128'.format(ed, ne)    # model dir
+    save_path = args.dataset + '_{}_{}_styleGAN_{}'.format(ed, ne, img_size)    # model dir
+    print(save_path)
+    root = '/home/jenny870207/data/' + args.dataset + '/'
+
+    # load data
+    train_data = dataset_unpair(root, 'train', 'A', 'B', img_size, img_size)
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, pin_memory=True)
+
+
+    f = os.path.join(os.getcwd(), save_path, 'settingc_latest.pt')
+    config = OmegaConf.load('config_settingc_with_styleGAN.yaml')
+    config.model.target = 'taming_comb.models.vqgan_patchstyleGAN.VQModelCrossGAN_StyleGAN'
+    config.model.base_learning_rate = learning_rate
+    config.model.params.embed_dim = ed
+    config.model.params.n_embed = ne
+    config.model.z_channels = args.z_channel
+    config.model.resolution = 256
+    model = instantiate_from_config(config.model)
+    if(os.path.isfile(f)):
+        print('load ' + f)
+        ck = torch.load(f, map_location=device)
+        model.load_state_dict(ck['model_state_dict'], strict=False)
+    model = model.to(device)
+    model.train()
+
+    # print(model.loss.discriminator)
+    
+    opt_ae = torch.optim.Adam(list(model.encoder.parameters())+
+                                list(model.decoder_a.parameters())+
+                                list(model.decoder_b.parameters())+
+                                list(model.quantize.parameters())+
+                                list(model.quant_conv.parameters())+
+                                list(model.post_quant_conv.parameters())+
+                                list(model.style_enc_a.parameters())+
+                                list(model.style_enc_b.parameters())+
+                                list(model.mlp_a.parameters())+
+                                list(model.mlp_b.parameters()),
+                                lr=learning_rate, betas=(0.5, 0.999))
+    
+    opt_disc_a = torch.optim.Adam(model.loss_a.discriminator.parameters(),
+                                lr=learning_rate, betas=(0.5, 0.999))
+    
+    opt_disc_b = torch.optim.Adam(model.loss_b.discriminator.parameters(),
+                                lr=learning_rate, betas=(0.5, 0.999))
+
+    opt_Dpatch_a = torch.optim.Adam(model.Dpatch_a.parameters(),
+                                lr=learning_rate, betas=(0.5, 0.999))
+
+    opt_Dpatch_b = torch.optim.Adam(model.Dpatch_b.parameters(),
+                                lr=learning_rate, betas=(0.5, 0.999))
+
+    if(os.path.isfile(f)):
+        print('load ' + f)
+        opt_ae.load_state_dict(ck['opt_ae_state_dict'])
+        opt_disc_a.load_state_dict(ck['opt_disc_a_state_dict'])
+        opt_disc_b.load_state_dict(ck['opt_disc_b_state_dict'])
+        opt_Dpatch_a.load_state_dict(ck['opt_Dpatch_a_state_dict'])
+        opt_Dpatch_b.load_state_dict(ck['opt_Dpatch_b_state_dict'])
+
+
+    if(not os.path.isdir(save_path)):
+        os.mkdir(save_path)
+
+    train_ae_a_error = []
+    train_ae_b_error = []
+    train_disc_a_error = []
+    train_disc_b_error = []
+    train_disc_a2b_error = []
+    train_disc_b2a_error = []
+    train_res_rec_error = []
+    
+    train_style_a_loss = []
+    train_style_b_loss = []
+    train_content_a_loss = []
+    train_content_b_loss = []
+
+    train_dpatch_a_loss = []
+    train_dpatch_b_loss = []
+    train_dpach_a_g_loss = []
+    train_dpach_b_g_loss = []
+
+    iterations = len(train_data) // batch_size
+    iterations = iterations + 1 if len(train_data) % batch_size != 0 else iterations
+    
+    
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    
+    for epoch in range(epoch_start, epoch_end+1):
+        for i in range(iterations):
+
+            dataA, dataB = next(iter(train_loader))
+            dataA, dataB = dataA.to(device), dataB.to(device)
+
+            
+            ## Discriminator A
+            opt_disc_a.zero_grad()
+            
+            s_a = model.encode_style(dataA, label=1)
+            fakeA, _, _ = model(dataB, label=0, cross=True, s_given=s_a)
+
+            recA, qlossA, _ = model(dataA, label=1, cross=False)
+            
+            b2a_loss, log = model.loss_a(_, dataA, fakeA, optimizer_idx=1, global_step=epoch,
+                                    last_layer=None, split="train")
+
+            a_rec_d_loss, _ = model.loss_a(_, dataA, recA, optimizer_idx=1, global_step=epoch,
+                                    last_layer=None, split="train")
+            
+            disc_a_loss = b2a_loss + 0.2*a_rec_d_loss
+            disc_a_loss.backward()
+            opt_disc_a.step()
+            
+            
+            ## Discriminator B
+            opt_disc_b.zero_grad()
+            
+            s_b = model.encode_style(dataB, label=0)
+            fakeB, _, s_b_sampled = model(dataA, label=1, cross=True, s_given=s_b)
+
+            recB, qlossB, _ = model(dataB, label=0, cross=False)
+            
+            a2b_loss, log = model.loss_b(_, dataB, fakeB, optimizer_idx=1, global_step=epoch,
+                                    last_layer=None, split="train")
+
+            b_rec_d_loss, _ = model.loss_b(_, dataB, recB, optimizer_idx=1, global_step=epoch,
+                                    last_layer=None, split="train")
+            
+          
+            disc_b_loss = a2b_loss + 0.2*b_rec_d_loss
+            disc_b_loss.backward()
+            opt_disc_b.step()
+
+
+            ## Dpatch A
+            opt_Dpatch_a.zero_grad()
+            dpatch_a_loss = model.compute_patch_discriminator_losses(dataA, fakeA, 'A')
+            dpatch_a_loss.backward(retain_graph=True)
+            opt_Dpatch_a.step()
+
+
+            ## Dpatch B
+            opt_Dpatch_b.zero_grad()
+            dpatch_b_loss = model.compute_patch_discriminator_losses(dataB, fakeB, 'B')
+            dpatch_b_loss.backward(retain_graph=True)
+            opt_Dpatch_b.step()
+
+            ## Generator 
+            opt_ae.zero_grad()
+                
+            # A reconstruction
+            #recA, qlossA, _ = model(dataA, label=1, cross=False)
+
+            aeloss_a, _ = model.loss_a(qlossA, dataA, recA, fake=fakeA, switch_weight=switch_weight, optimizer_idx=0, global_step=epoch,
+                                    last_layer=model.get_last_layer(label=1), split="train")
+                                    
+            aeloss_a = aeloss_a.to(device)
+            
+            # cross path with style a
+            AtoBtoA, _, s_a_from_cross = model(fakeA, label=1, cross=False)
+            
+            # style_a loss
+            style_a_loss = torch.mean(torch.abs(s_a.detach() - s_a_from_cross)).to(device)
+            
+            # content_b loss
+            c_b_from_cross, _ = model.encode_content(fakeA)
+            _, quant_c_b = model.encode_content(dataB)
+            content_b_loss = torch.mean(torch.abs(quant_c_b.detach() - c_b_from_cross)).to(device)
+            
+            
+            # B reconstruction
+            #recB, qlossB, _ = model(dataB, label=0, cross=False)
+
+            aeloss_b, _ = model.loss_b(qlossB, dataB, recB, fake=fakeB, switch_weight=switch_weight, optimizer_idx=0, global_step=epoch,
+                                    last_layer=model.get_last_layer(label=0), split="train")
+            
+            aeloss_b = aeloss_b.to(device)
+            
+            # cross path with style b
+            BtoAtoB, _, s_b_from_cross = model(fakeB, label=0, cross=False)
+             
+            # style_b loss
+            style_b_loss = torch.mean(torch.abs(s_b.detach() - s_b_from_cross)).to(device)
+            
+             # content_a loss
+            c_a_from_cross, _ = model.encode_content(fakeB)
+            _, quant_c_a = model.encode_content(dataA)
+            content_a_loss = torch.mean(torch.abs(quant_c_a.detach() - c_a_from_cross)).to(device)
+            
+            
+            # patch style GAN loss
+            dpach_a_g_loss = model.compute_Dpatch_G_loss(dataA, fakeA, 'A')
+            dpach_b_g_loss = model.compute_Dpatch_G_loss(dataB, fakeB, 'B')
+
+
+            gen_loss = aeloss_a + aeloss_b + 1.0*(style_a_loss + style_b_loss) + 0.2*(content_a_loss + content_b_loss) + 0.5*(dpach_a_g_loss + dpach_b_g_loss)
+            gen_loss.backward()
+            opt_ae.step()
+            
+            
+
+            # compute mse loss b/w input and reconstruction
+            data = torch.cat((dataA, dataB), 0).to(device)
+            rec = torch.cat((recA, recB), 0).to(device)
+            recon_error = F.mse_loss( data, rec)
+
+            train_res_rec_error.append(recon_error.item())
+            train_ae_a_error.append(aeloss_a.item())
+            train_ae_b_error.append(aeloss_b.item())
+            train_disc_a_error.append(disc_a_loss.item())
+            train_disc_b_error.append(disc_b_loss.item())
+            train_disc_a2b_error.append(a2b_loss.item())
+            train_disc_b2a_error.append(b2a_loss.item())
+            
+            train_style_a_loss.append(style_a_loss.item())
+            train_style_b_loss.append(style_b_loss.item())
+            
+            train_content_a_loss.append(content_a_loss.item())
+            train_content_b_loss.append(content_b_loss.item())
+
+            train_dpatch_a_loss.append(dpatch_a_loss.item())
+            train_dpatch_b_loss.append(dpatch_b_loss.item())
+            train_dpach_a_g_loss.append(dpach_a_g_loss.item())
+            train_dpach_b_g_loss.append(dpach_b_g_loss.item())
+            
+
+            if (i+1) % 1000 == 0:
+                _rec  = 'epoch {}, {} iterations\n'.format(epoch, i+1)
+                _rec += '(A domain) ae_loss: {:8f}, disc_loss: {:8f}\n'.format(
+                            np.mean(train_ae_a_error[-1000:]), np.mean(train_disc_a_error[-1000:]))
+                _rec += '(B domain) ae_loss: {:8f}, disc_loss: {:8f}\n'.format(
+                            np.mean(train_ae_b_error[-1000:]), np.mean(train_disc_b_error[-1000:]))
+                _rec += 'A vs A2B loss: {:8f}, B vs B2A loss: {:8f}\n'.format(
+                            np.mean(train_disc_a2b_error[-1000:]), np.mean(train_disc_b2a_error[-1000:]))
+                _rec += 'recon_error: {:8f}\n\n'.format(
+                    np.mean(train_res_rec_error[-1000:]))
+                
+                _rec += 'style_a_loss: {:8f}\n\n'.format(
+                    np.mean(train_style_a_loss[-1000:]))
+                _rec += 'style_b_loss: {:8f}\n\n'.format(
+                    np.mean(train_style_b_loss[-1000:]))
+                
+                _rec += 'content_a_loss: {:8f}\n\n'.format(
+                    np.mean(train_content_a_loss[-1000:]))
+                _rec += 'content_b_loss: {:8f}\n\n'.format(
+                    np.mean(train_content_b_loss[-1000:]))
+
+                _rec += 'dpatch_a_loss: {:8f}\n\n'.format(
+                    np.mean(train_dpatch_a_loss[-1000:]))
+                _rec += 'dpatch_b_loss: {:8f}\n\n'.format(
+                    np.mean(train_dpatch_b_loss[-1000:]))
+                _rec += 'dpatch_a_g_loss: {:8f}\n\n'.format(
+                    np.mean(train_dpach_a_g_loss[-1000:]))
+                _rec += 'dpatch_b_g_loss: {:8f}\n\n'.format(
+                    np.mean(train_dpach_b_g_loss[-1000:]))
+                
+                print(_rec)
+                with open(os.path.join(os.getcwd(), save_path, 'loss.txt'), 'a') as f:
+                    f.write(_rec)
+                    f.close()
+            
+        torch.save(
+            {
+                'model_state_dict': model.state_dict(),
+                'opt_ae_state_dict': opt_ae.state_dict(),
+                'opt_disc_a_state_dict': opt_disc_a.state_dict(),
+                'opt_disc_b_state_dict': opt_disc_b.state_dict(),
+                'opt_Dpatch_a_state_dict': opt_Dpatch_a.state_dict(),
+                'opt_Dpatch_b_state_dict': opt_Dpatch_b.state_dict()
+            }, os.path.join(os.getcwd(), save_path, 'settingc_latest.pt'))
+
+
+        if(epoch % 20 == 0 and epoch >= 20):
+            torch.save(
+                {
+                    'model_state_dict': model.state_dict(),
+                    'opt_ae_state_dict': opt_ae.state_dict(),
+                    'opt_disc_a_state_dict': opt_disc_a.state_dict(),
+                    'opt_disc_b_state_dict': opt_disc_b.state_dict(),
+                    'opt_Dpatch_a_state_dict': opt_Dpatch_a.state_dict(),
+                    'opt_Dpatch_b_state_dict': opt_Dpatch_b.state_dict()
+                }, os.path.join(os.getcwd(), save_path, 'settingc_n_{}.pt'.format(epoch)))
+            # torch.save(
+            #     {
+            #         'model_state_dict': model.state_dict(),
+            #         'opt_ae_state_dict': opt_ae.state_dict(),
+            #         'opt_disc_a_state_dict': opt_disc_a.state_dict(),
+            #         'opt_disc_b_state_dict': opt_disc_b.state_dict()
+            #     }, os.path.join(os.getcwd(), save_path, 'vqgan_latest.pt'))
+
