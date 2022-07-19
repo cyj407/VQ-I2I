dfe05769ce23ede9e717b958e7cc74a9e0664713
cyj407 <dfghj345678@gmail.com>
2022-07-19 18:23:08 +0800

    Remove redundant code in `save_transfer.py`

--- save_transfer.py
+++ save_transfer.py
@@ -1,86 +1,18 @@
-from __future__ import print_function
-from genericpath import exists
-import torch
-import numpy as np
-from PIL import Image
-import os
+import torch, os, random, argparse
 from dataset import dataset_single
 from torch.utils.data import DataLoader
-import sys
-import matplotlib.pyplot as plt
-np.set_printoptions(threshold=sys.maxsize)
-from torchvision.utils import make_grid
-import matplotlib as mpl
-import random
-import importlib
-import argparse
-
-mpl.rcParams.update({'figure.max_open_warning': 0})
-
+from omegaconf import OmegaConf
+from utils import instantiate_from_config, save_tensor
 from taming_comb.modules.style_encoder.network import *
 from taming_comb.modules.diffusionmodules.model import * 
 
-def get_obj_from_str(string, reload=False):
-    module, cls = string.rsplit(".", 1)
-    if reload:
-        module_imp = importlib.import_module(module)
-        importlib.reload(module_imp)
-    return getattr(importlib.import_module(module, package=None), cls)
-
-
-def instantiate_from_config(config):
-    if not "target" in config:
-        raise KeyError("Expected key `target` to instantiate.")
-    return get_obj_from_str(config["target"])(**config.get("params", dict()))
-
-def tensor2im(input_image, imtype=np.uint8):
-    """"Converts a Tensor array into a numpy image array.
-
-    Parameters:
-        input_image (tensor) --  the input image tensor array
-        imtype (type)        --  the desired type of the converted numpy array
-    """
-    if not isinstance(input_image, np.ndarray):
-        if isinstance(input_image, torch.Tensor):  # get the data from a variable
-            image_tensor = input_image.data
-        else:
-            return input_image
-        image_numpy = image_tensor[0].clamp(-1.0, 1.0).cpu().float().numpy()  # convert it into a numpy array
-        if image_numpy.shape[0] == 1:  # grayscale to RGB
-            image_numpy = np.tile(image_numpy, (3, 1, 1))
-        image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0  # post-processing: tranpose and scaling
-    else:  # if it is a numpy array, do nothing
-        image_numpy = input_image
-    return image_numpy.astype(imtype)
-
-
-def save_image(image_numpy, image_path, aspect_ratio=1.0):
-    """Save a numpy image to the disk
-
-    Parameters:
-        image_numpy (numpy array) -- input numpy array
-        image_path (str)          -- the path of the image
-    """
-
-    image_pil = Image.fromarray(image_numpy)
-    # h, w, _ = image_numpy.shape
-
-    # if aspect_ratio is None:
-    #     pass
-    # elif aspect_ratio > 1.0:
-    #     image_pil = image_pil.resize((h, int(w * aspect_ratio)), Image.BICUBIC)
-    # elif aspect_ratio < 1.0:
-    #     image_pil = image_pil.resize((int(h / aspect_ratio), w), Image.BICUBIC)
-    image_pil.save(image_path)
 
 
 def load_eval_model(_path, config_file, ed, ne, target, z):
-    from omegaconf import OmegaConf
     config = OmegaConf.load(config_file)
     # print(config.model.params.n_embed)
     config.model.target = target
     config.model.z_channels = z
-    config.model.resolution = 256
     config.model.params.n_embed = ne
     config.model.params.embed_dim = ed
     model_a = instantiate_from_config(config.model)
@@ -89,12 +21,6 @@ def load_eval_model(_path, config_file, ed, ne, target, z):
     model_a.load_state_dict(ck['model_state_dict'], strict=False)
     model_a = model_a.to(device)
     return model_a.eval()
-
-
-def save_tensor(im_data, image_dir, image_name):
-    im = tensor2im(im_data)
-    save_path = os.path.join(image_dir, str(image_name)) #+ '.png'
-    save_image(im, save_path)
 
 
 if __name__ == "__main__":
@@ -142,7 +68,7 @@ if __name__ == "__main__":
                     help="embedding dimension",
                     type=int)
 
-    parser.add_argument("--z_channel",default=128,
+    parser.add_argument("--z_channel",default=256,
                     help="z channel",
                     type=int)
 
